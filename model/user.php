c96cb62dffb23334867466aca630cd8f6a185d8c
Alex <Canikpro@mail.ru>
2016-02-18 08:59:35 +0300

    edit arguments sql_inner

--- model/user.php
+++ model/user.php
@@ -2,13 +2,15 @@
 
 class User {
 
-  private static function sql_inner($login, $password, $sql) {
+  private static function sql_inner($login, $sql) {
     $db = Db::get_connection();
     $db->exec('SET CHARACTER SET utf8');
 
     $result = $db->prepare($sql);
     $result->bindParam(':login', $login, PDO::PARAM_INT);
-    $result->bindParam(':password', $password, PDO::PARAM_INT);
+    if (func_num_args() > 1) {
+      $result->bindParam(':password', func_get_arg(2), PDO::PARAM_INT);
+    }
     if (func_num_args() > 3) {
       $result->bindParam(':first_name', func_get_arg(3), PDO::PARAM_INT);
       $result->bindParam(':last_name', func_get_arg(4), PDO::PARAM_INT);
@@ -21,7 +23,7 @@ class User {
   public static function login($login, $password) {
 
     $sql = "SELECT * FROM users WHERE login = :login and password = :password";
-    $result = User::sql_inner($login, $password, $sql);
+    $result = User::sql_inner($login, $sql, $password);
 
     $user = $result->fetch();
     if ($user) {
@@ -31,8 +33,8 @@ class User {
 
   public static function register($login, $password, $first_name, $last_name) {
 
-    $sql = "SELECT * FROM users WHERE login = :login and password = :password";
-    $result = User::sql_inner($login, $password, $sql);
+    $sql = "SELECT * FROM users WHERE login = :login";
+    $result = User::sql_inner($login, $sql, $password);
 
     $user = $result->fetch();
     if ($user) {
@@ -40,7 +42,7 @@ class User {
     } else {
       $sql = "INSERT INTO users (login, password, first_name, last_name)
               VALUES (:login, :password, :first_name, :last_name)";
-      $result = User::sql_inner($login, $password, $sql, $first_name, $last_name);
+      $result = User::sql_inner($login, $sql, $password, $first_name, $last_name);
       return $result;
     }
   }
