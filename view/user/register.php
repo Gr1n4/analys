d97017aba9518a868b277ab38509e15df3ed47d1
Alex <Canikpro@mail.ru>
2016-04-26 06:38:51 +0300

    form register new

--- view/user/register.php
+++ view/user/register.php
@@ -1,15 +1,18 @@
 <?php include_once dir . '/view/layout/header.php'; ?>
 
 <body>
-  <p>Register</p>
+  <h1>Регистрация</h1>
   <?php if ($errors) {
     echo "<p>$errors[0]</p>";
   } ?>
   <form action="" method="post">
-    <input type="text" name="login" placeholder="Login">
-    <input type="text" name="first_name" placeholder="First name">
-    <input type="text" name="last_name" placeholder="Last name">
-    <input type="password" name="password" placeholder="Password">
+    <input type="text" name="login" placeholder="Логин">
+    <input type="password" name="password" placeholder="Пароль">
+    <input type="text" name="surname" placeholder="Фамилия">
+    <input type="text" name="first_name" placeholder="Имя">
+    <input type="text" name="last_name" placeholder="Отчество">
+    <input type="text" name="index" placeholder="Индекс">
+    <input type="text" name="address" placeholder="Адрес">
     <input type="submit" name="check_out">
   </form>
 </body>
